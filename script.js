39f401b7169044d1d6afa2b11854bf3c93f4db26
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-07-01 19:33:47 +0100

    Add DOM LOADED event listener. Add click event listener to submit button for scenario selector. Add ids and values for submit button and scenario options. Set up conditional statement for different scenario options

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+document.addEventListener("DOMContentLoaded", () =>{
+    const submitButton = document.getElementById("submitBtn")
+    const ScenarioSelector = document.querySelector(".scenario-selector")
+    submitButton.addEventListener("click", ()=>{
+        if (ScenarioSelector.value == "Flatiron Campus"){
+            getProgrammingJoke(){
+
+            }
+        }
+    })
+
+
+
+})
+
+
+function getProgrammingJoke(){
+    fetch('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,racist,sexist,explicit')
+    .then(resp=>{
+        console.log(resp.status)
+        console.log(resp.ok)
+        return resp.json()
+    })
+}
+
+
