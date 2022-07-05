7a4e510e43c38cb640d5533679f81da1035444ad
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-07-05 12:09:44 +0100

    checkbox now changes form innerHTML to take a single-line joke input. adapted script.js to take input from both forms, using conditional in handleSubmit. About to make checkbox revert to original form

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ document.addEventListener("DOMContentLoaded", () =>{
     const select = document.getElementById("select");
     const randomJoke= document.getElementById("random");
     const clearList = document.getElementById("clear");
-    let container = document.getElementById("main-joke-container")
-    const form = document.getElementById("jokeInputForm")
+    let container = document.getElementById("main-joke-container");
+    const form = document.getElementById("jokeInputForm");
+    const singleJokeBox= document.getElementById("single-joke-checkbox")
+
+    singleJokeBox.addEventListener('change', ()=>{
+        changeToSingleForm()
+    })
 
     clearList.addEventListener("click", ()=>{
         container.innerHTML = '';
@@ -30,6 +35,60 @@ document.addEventListener("DOMContentLoaded", () =>{
 
 })
 
+function changeToSingleForm(){
+    let form = document.getElementById("jokeInputForm");
+    form.innerHTML = '';
+    let jokeInputLabel = document.createElement("label");
+    let jokeInput = document.createElement("input");
+    let jokeSubmit = document.createElement("input");
+    let doubleJokeBox = document.createElement("input");
+    
+    jokeInputLabel.innerText = "Put in your one-liner!"
+    jokeInput.id = "single-joke-input"
+    jokeSubmit.setAttribute("type", "submit")
+    jokeSubmit.id = "submitBtn"
+    doubleJokeBox.setAttribute("type", "checkbox")
+    doubleJokeBox.id = ("double-joke-checkbox")
+
+    form.append(jokeInputLabel, jokeInput, jokeSubmit, doubleJokeBox)
+}
+
+
+
+
+function handleSubmit(e){
+    e.preventDefault();
+    if (document.getElementById("single-joke-input")!== null){
+        let oneLiner = document.getElementById("single-joke-input")
+        let jokeObj = {
+            
+                category: "userSubmitted",
+                type: "single",
+                joke: oneLiner.value,
+                safe: true,
+                lang: "en",
+        }
+        postNewJoke(jokeObj);
+        oneLiner.value = ""
+    }
+    else {
+        let buildUp = document.getElementById("build-up");
+        let punchline = document.getElementById("punchline");
+        let jokeObj = {
+            category: "userSubmitted",
+            type: "twopart",
+            setup: buildUp.value,
+            delivery: punchline.value,
+            safe: true,
+            lang: "en",
+
+        }
+        postNewJoke(jokeObj)
+        buildUp.value = ''
+        punchline.value = ''}
+
+}
+
 function postNewJoke(jokeObj){
     fetch('http://localhost:3000/jokes', {
         method: "POST",
@@ -39,44 +98,22 @@ function postNewJoke(jokeObj){
         body: JSON.stringify(jokeObj)
     })
     .then(resp => resp.json())
-    .then(joke => console.log(joke))
+    .then(joke => appendJoke(joke))
 }
 
+// ALLOW USERS TO TOGGLE WHETHER THEIR JOKE IS A SINGLE LINE OR A SET UP AND PUNCH LINE STYLE JOKE
 
+//- add event listener
 
-function handleSubmit(e){
-    e.preventDefault();
-    let buildUp = document.getElementById("build-up");
-    let punchline = document.getElementById("punchline");
-    let jokeObj = {
-        category: "userSubmitted",
-        type: "twopart",
-        setup: buildUp.value,
-        delivery: punchline.value,
-        safe: true,
-        lang: "en",
+//- change inner html of form
 
-    }
-    postNewJoke(jokeObj)
-    buildUp.value = ''
-    punchline.value = ''
-
-}
-
-
-//WRITE JOKE GETTER AS A CALLBACK AND PASS INTO ALL JOKE GETTERS TO MAKE CODE DRIER- DONE
-
-//NEXT SET UP JSON SERVER AND USE THAT INSTEAD OF API
-
-// GET SUBMIT OWN JOKE FORM WORKING
-
-// GET RANDOM JOKE BUTTON WORKING- DONE
+//- add event listener to new form that makes process reversible
 
 //STRETCH-  USE ARRAY METHOD TO PREVENT REPEAT JOKES
 
 //STRETCH- HAVE AN ALERT MESSAGE THAT PLAYS WHEN A MAXIMUM NUMBER OF SPOOKY JOKES HAS BEEN PUT ON THE DOM
 
-// Have the Opportunity to like jokes and put them on a favourite list
+//STRETCH- GIVE OPPORTUNITY TO LIKE JOKES AND ADD THEM TO A FAVOURITE LIST
 
 function getProgrammingJoke(){
     fetch('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,racist,sexist,explicit')
